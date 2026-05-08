f59c697a4f126deb56449e91612de4a4d610cdfc
Gabriel Augusto <ag71gabriel@gmail.com>
2026-05-08 14:55:45 -0300

    chore: add .env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const subtrackrAPI = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
   headers: { 'Content-Type': 'application/json' }
 });
